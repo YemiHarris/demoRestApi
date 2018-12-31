67c3d947c0d4282c8520f748cb8fff187091d0c4
Olayemi Harris <yemih@amazon.com>
2018-12-31 11:31:48 -0600

    station service implementation boilerplate

---
+++ src/main/java/com/yemi/demo/api/service/StationServiceImpl.java
@@ -0,0 +1,35 @@
+package com.yemi.demo.api.service;
+
+import com.yemi.demo.api.model.Station;
+
+public class StationServiceImpl implements StationService {
+    @Override
+    public Station findById(String stationId) {
+        return null;
+    }
+
+    @Override
+    public Station findByName(String name) {
+        return null;
+    }
+
+    @Override
+    public Station findByHdEnabled(Boolean hdEnabled) {
+        return null;
+    }
+
+    @Override
+    public void saveStation(Station station) {
+
+    }
+
+    @Override
+    public void updateStation(Station station) {
+
+    }
+
+    @Override
+    public void deleteStation(String stationId) {
+
+    }
+}
