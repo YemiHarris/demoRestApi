81154e4f2f43c6d10ca8d948c0f854bfd6248be7
Olayemi Harris <yemih@amazon.com>
2018-12-31 11:29:14 -0600

    added station service interface

--- src/main/java/com/yemi/demo/api/service/StationService.java
+++ src/main/java/com/yemi/demo/api/service/StationService.java
@@ -1,4 +1,17 @@
 package com.yemi.demo.api.service;
 
-public class StationService {
+import com.yemi.demo.api.model.Station;
+
+public interface StationService {
+    Station findById(String stationId);
+
+    Station findByName(String name);
+
+    Station findByHdEnabled(Boolean hdEnabled);
+
+    void saveStation(Station station);
+
+    void updateStation(Station station);
+
+    void deleteStation(String stationId);
 }
