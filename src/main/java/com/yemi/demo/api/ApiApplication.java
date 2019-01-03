99805bcc1bfdcf36cf7af143bfa20cbccae6c164
Olayemi Harris <yemih@amazon.com>
2019-01-03 10:50:29 -0600

    added annotations to enable swagger ui as well as jpa repositories

--- src/main/java/com/yemi/demo/api/ApiApplication.java
+++ src/main/java/com/yemi/demo/api/ApiApplication.java
@@ -2,7 +2,11 @@ package com.yemi.demo.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
+@EnableJpaRepositories("com.yemi.demo.api.repository")
 @SpringBootApplication
 public class ApiApplication {
 
