e81ff5ed107200d9baef2582579a4b3e6d3afd73
Olayemi Harris <yemih@amazon.com>
2019-01-06 18:36:26 -0600

    test scaffolds

---
+++ src/test/java/com/yemi/demo/api/controller/StationControllerTest.java
@@ -0,0 +1,56 @@
+package com.yemi.demo.api.controller;
+
+import com.yemi.demo.api.AbstractTest;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StationControllerTest extends AbstractTest {
+
+    @Override
+    @Before
+    public void setUp() {
+        super.setUp();
+    }
+
+    @Test
+    public void getStationById() throws Exception {
+        //arrange
+        //act
+        //assert
+    }
+
+    @Test
+    public void getStationByName() throws Exception {
+        //arrange
+        //act
+        //assert
+    }
+
+    @Test
+    public void getStation() throws Exception {
+        //arrange
+        //act
+        //assert
+    }
+
+    @Test
+    public void createStation() throws Exception {
+        //arrange
+        //act
+        //assert
+    }
+
+    @Test
+    public void updateStation() throws Exception {
+        //arrange
+        //act
+        //assert
+    }
+
+    @Test
+    public void deleteStation() throws Exception {
+        //arrange
+        //act
+        //assert
+    }
+}
